9a7a5f38c096be37e5c9beccb0f488cbfb64ad8d
Murilo <64508814+MuriloBetoni@users.noreply.github.com>
2023-02-03 15:20:41 -0300

    Curso Sistemas de informação concluído

--- CV Murilo.py
+++ CV Murilo.py
@@ -18,7 +18,7 @@ phone, email, linkedin = contact
 
 objective = "Estágio - Desenvolvedor"
 
-academic_education = {'semestre':6, 'curso':'Sistemas de Informação', 'faculdade':'Universidade Metodista de São Paulo', 'conclusao':'12/2022'}
+academic_education = {'curso':'Sistemas de Informação', 'faculdade':'Universidade Metodista de São Paulo', 'conclusao':'12/2022'}
 
 professional_xp = {'cargo':'Assistente Administrativo', 'empresa':'4 Show Comércio de Eletrônicos', 'admissao':f'{calendar.month_abbr[12]}/{int(year) - 1}', 'demissao':f'{calendar.month_abbr[7]}/{year}', 'atv':'Testes de funcionalidades para celulares, limpeza de eletrônicos e trabalhos manuais de carga e descarga'}
 
@@ -34,7 +34,7 @@ print(f"{phone} \n{email} \n{linkedin} \n")
 
 print(f"{negrito}Objetivo{limpa} \n{objective} \n")
 
-print(f"{negrito}Formação{limpa} \nCursando {academic_education['semestre']}º semestre de {academic_education['curso']} \n{academic_education['faculdade']} \nConclusão em {academic_education['conclusao']} \n")
+print(f"{negrito}Formação{limpa} \nBacharelado em {academic_education['curso']} \n{academic_education['faculdade']} \nConcluído em {academic_education['conclusao']} \n")
 
 print(f"{negrito}Experiência Profissional{limpa} \n{professional_xp['cargo']} \n{professional_xp['empresa']} \n{professional_xp['admissao']} - {professional_xp['demissao']} \n\nAtividades realizadas: \n{professional_xp['atv']} \n")
 
